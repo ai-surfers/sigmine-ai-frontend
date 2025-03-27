a7af52333a45fab347283d0c73e0b812aaccd790
전해연 <93465128+hailey-hy@users.noreply.github.com>
2025-03-27 15:05:40 +0900

    [세팅] git, tanstack query, recoil, styled-components 초기 세팅 (#1)
    * chore: git 초기 세팅
    
    * fix: styled-components 재설치
    
    * feat: react query, recoil, styled-components 초기 세팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import ReactQueryProvider from "./providers/ReactQueryProvider";
+import RecoilProvider from "./providers/RecoilProvider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +16,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ReactQueryProvider>
+          <RecoilProvider>{children}</RecoilProvider>
+        </ReactQueryProvider>
       </body>
     </html>
   );
