e8c1d72b441af50f328bf946fc6ce077278f0dee
hailey-hy <lillyine.hy@gmail.com>
2025-04-09 13:23:16 +0900

    chore: 환경 변수 등록

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 const nextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    NEXT_PUBLIC_WEB_URL: process.env.NEXT_PUBLIC_WEB_URL,}
 };
 
 export default nextConfig;
